081a4f328641f27f12fc9f4b2738ebeb8f352902
jeremyknight <jeremyknightjck99@gmail.com>
2019-04-04 23:14:35 -0700

    started blob problem.  ran into trying to fix the blob problem.

--- headers/BlobDetection.h
+++ headers/BlobDetection.h
@@ -14,10 +14,55 @@ class BlobDetection {
         BlobDetection(string fn) {
             fileName = fn;
             ifstream file = ifstream(fn);
-            while(file >> c) {
+            string line;
+            while(file >> line) {
+                vector<int> temp;
+                for(int i = 0; i < line.length(); i++) {
+                    temp.push_back(line.at(i) - '0');
+                }
+                arr.push_back(temp);
+            }
+        }
+
+        void printArray() {
+            
+            for(int x = 0; x<arr.size(); x++) {
+                for(int y = 0; y<arr[x].size(); y++) {
+                    cout << arr[x][y];
+                }
+                cout << endl;
+            }
+            
+        }
+
+        void findBlobs() {
+            int num = 2;
+            for(int x = 0; x<arr.size(); x++) {
+                for(int y = 0; y<arr[x].size(); y++) {
+                    if(checkBlob(x,y,num)) {
+                        num++;
+                    }
+                }
                 
             }
         }
+
+        bool checkBlob(int x, int y, int num) {
+            if(x > arr.size() || x < 0 || y>arr[x].size() || y<0) { return false; }
+            if(arr[x][y] == 1) {
+                arr[x][y] = num;
+                //for()
+            } 
+
+            if(arr[x]][y] == num) {
+                //return true;
+            } 
+
+            return false;
+            
+        }
+
+
 };
 
 #endif
--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "Queen.h"
+#include "BlobDetection.h"
 using namespace std;
 
 int main() {
+    /*
     Queen q= Queen();
     q.printBoard();
     //while(q.queenPlace(0,0))
@@ -11,6 +14,12 @@ int main() {
         cout << "this worked" << endl;
     }
     q.printBoard();
+    */
+
+    //string file name:
+    string fileName = "C:/Users/jknight22/desktop/cs273-project4-JeremyKnight/blob_test.txt";
+    BlobDetection blob = BlobDetection(fileName);
+    blob.printArray();
 
     return 1;
 }
